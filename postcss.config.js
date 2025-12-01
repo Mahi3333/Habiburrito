16d1d0000a86472c39e4ce33c4177440a285b3b7
Mahi3333 <mumamahesh13@gmail.com>
2025-11-30 22:04:32 -0500

    FIX: Overrode PostCSS config to explicitly use @tailwindcss/postcss to resolve fatal build error

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    plugins: [
-        'tailwindcss',
-        'autoprefixer',
-    ],
+    plugins: {
+        // Explicitly referencing the required adapter package
+        '@tailwindcss/postcss': {},
+        'autoprefixer': {},
+    },
 }
