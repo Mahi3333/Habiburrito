300d306fabb8fcf6869d9b5b2f1d0e75b7093a75
Mahi3333 <mumamahesh13@gmail.com>
2026-01-11 23:44:27 -0500

    VERSION-2: updated the build errors

--- prisma.config.ts
+++ prisma.config.ts
@@ -13,7 +13,4 @@ export default defineConfig({
   datasource: {
     url: env("DATABASE_URL"),
   },
-  seeds: {
-    seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
-  },
 });
